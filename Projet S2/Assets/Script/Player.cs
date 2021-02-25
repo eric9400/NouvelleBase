0debb205704e1188be77335d26023684541f33fe
eric9400 <eric.baysengchanh@epita.fr>
2021-02-25 12:17:52 +0100

    Partie réseau qui avance
    Partie réseau bien avancé

---
+++ Projet S2/Assets/Script/Player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : Photon.MonoBehaviour
+{
+    public new PhotonView photonView;
+    public Rigidbody2D rb;
+    public Animator anim;
+    public GameObject PlayerCamera;
+    public SpriteRenderer sr;
+    public Text PlayerNameText;
+
+    public bool IsGrounded = false;
+    public float MoveSpeed;
+    public float JumpForce;
+
+    private void Awake()
+    {
+        if (photonView.isMine)
+        {
+            PlayerCamera.SetActive(true);
+        }
+    }
+
+    private void Update()
+    {
+        if(photonView.isMine)
+        {
+            CheckInput();
+        }
+    }
+
+    private void CheckInput()
+    {
+        var move = new Vector3(Input.GetAxisRaw("Horizontal"), 0);
+        transform.position += move * MoveSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            sr.flipX = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            sr.flipX = false;
+        }
+    }
+}
